20c1177bf5f0ab9ff4fdfae4895499440efa6f4a
Henry Jetmundsen <henryjetmundsen@Henrys-MBP.home>
2020-12-13 15:02:31 -0500

    Removed debugging from day4 py

--- src/2020/python/src/day04.py
+++ src/2020/python/src/day04.py
@@ -9,25 +9,6 @@ def _validate_fields(fields, counter):
     ecl = fields["ecl"]
     pid = fields["pid"]
 
-    if not (
-        byr.isdigit()
-        and 1920 <= int(byr) <= 2002
-        and iyr.isdigit()
-        and 2010 <= int(iyr) <= 2020
-        and eyr.isdigit()
-        and 2020 <= int(eyr) <= 2030
-        and (
-            (hgt[-2:] == "cm" and hgt[0:-2].isdigit() and 150 <= int(hgt[0:-2]) <= 193)
-            or (hgt[-2:] == "in" and hgt[0:-2].isdigit() and 59 <= int(hgt[0:-2]) <= 76)
-        )
-        and hcl[0] == "#"
-        and all([(x.isdigit() or x in hex_chars) for x in hcl[1:]])
-        and ecl in eye_colors
-        and len(pid) == 9
-        and pid.isdigit()
-    ):
-        print(counter)
-
     return (
         byr.isdigit()
         and 1920 <= int(byr) <= 2002
