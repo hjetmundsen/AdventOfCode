f3fafda01e0ed69117fa68bd4475c69005ca48f7
Henry Jetmundsen <henryjetmundsen@Henrys-MBP.home>
2020-12-15 01:21:22 -0500

    Cleanup

--- src/2020/python/src/main.py
+++ src/2020/python/src/main.py
@@ -1,18 +1,26 @@
 import solutions
 
-day = input("Which day would you like to run (1-7): ")
 
-if day == "1":
-    solutions.day01()
-elif day == "2":
-    solutions.day02()
-elif day == "3":
-    solutions.day03()
-elif day == "4":
-    solutions.day04()
-elif day == "5":
-    solutions.day05()
-elif day == "6":
-    solutions.day06()
-elif day == "7":
-    solutions.day07()
+while True:
+    day = input("Which day would you like to run (1-7): ")
+
+    if day == "exit":
+        break
+    elif day == "1":
+        solutions.day01()
+    elif day == "2":
+        solutions.day02()
+    elif day == "3":
+        solutions.day03()
+    elif day == "4":
+        solutions.day04()
+    elif day == "5":
+        solutions.day05()
+    elif day == "6":
+        solutions.day06()
+    elif day == "7":
+        solutions.day07()
+    elif day == "8":
+        solutions.day08()
+    else:
+        print("Invalid input")
--- src/2020/rust/src/day_07.rs
+++ src/2020/rust/src/day_07.rs
@@ -1,7 +1,17 @@
 use crate::utils;
+use regex::Regex;
 
 fn star1() -> u32 {
-    0
+    let lines = utils::file_to_lines("../inputs/day7.txt");
+    let re = Regex::new("(.*) bags contain").unwrap();
+    
+    for line in &lines {
+        dbg!(line);
+        let bag: Vec<&str> = re.split(line).collect();
+        dbg!(bag);
+    }
+
+    return 0;
 }
 
 fn star2() -> u32 {
