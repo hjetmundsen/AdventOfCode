bd201fd5869538e0ec2e09c245a81df82eb6bdf7
Henry Jetmundsen <hjetmundsen@atlassian.com>
2021-12-03 02:38:55 -0500

    clean day 3 2021

--- src/2021/python/src/day3/main.py
+++ src/2021/python/src/day3/main.py
@@ -1,68 +1,38 @@
-def _star1():
+def star1():
     inp = [x.strip() for x in open("./input", "r")]
-
-    gamma = ""
-    epsilon = ""
-
+    gamma, epsilon = "", ""
 
     for i in range(0, 12):
-        g, e = star1_helper(inp, i)
-        gamma += g
-        epsilon += e
+        g, e = _gamma_epsilon_helper(inp, i)
+        gamma, epsilon = (gamma + g, epsilon + e)
 
     return int(gamma, 2) * int(epsilon, 2)
 
 
-def star1_helper(inp, val):
-    x = 0
-    for i in inp:
-        if i[val] == "1":
-            x += 1
-
-    if x > len(inp) / 2:
-        return ("1", "0")
-    return ("0", "1")
+def _gamma_epsilon_helper(inp, val):
+    x = len([i for i in inp if i[val] == "1"])
+    return ("1", "0") if x > len(inp) // 2 else ("0", "1")
 
 
-def _star2():
+def star2():
     oxy = [x.strip() for x in open("./input", "r")]
     carb = [x.strip() for x in open("./input", "r")]
     
     for i in range(0, 12):
-        if len(oxy) > 1:
-            oxy = oxygen(oxy, i)
-        if len(carb) > 1:
-            carb = carbon(carb, i)
+        oxy = _oxygen_helper(oxy, i) if len(oxy) > 1 else oxy
+        carb = _carbon_helper(carb, i) if len(carb) > 1 else carb
 
     return int(oxy[0], 2) * int(carb[0], 2)
 
 
-def oxygen(inp, val):
-    num1 = 0
-    num0 = 0
-    for i in inp:
-        if i[val] == "1":
-            num1 += 1
-        else:
-            num0 += 1
-
-    if num0 > num1:
-        return [x for x in inp if x[val] == "0"]
-    return [x for x in inp if x[val] == "1"]
+def _oxygen_helper(inp, val):
+    x = len([i for i in inp if i[val] == "1"])
+    return [x for x in inp if x[val] == "1"] if x >= len(inp) - x else [x for x in inp if x[val] == "0"]
 
 
-def carbon(inp, val):
-    num1 = 0
-    num0 = 0
-    for i in inp:
-        if i[val] == "1":
-            num1 += 1
-        else:
-            num0 += 1
-
-    if num1 >= num0:
-        return [x for x in inp if x[val] == "0"]
-    return [x for x in inp if x[val] == "1"]
+def _carbon_helper(inp, val):
+    x = len([i for i in inp if i[val] == "1"])
+    return [x for x in inp if x[val] == "1"] if x < len(inp) - x else [x for x in inp if x[val] == "0"]
 
 
-print("DAY 1\n=====\nSTAR 1: {}\nSTAR 2: {}\n".format(_star1(), _star2()))
+print("DAY 1\n=====\nSTAR 1: {}\nSTAR 2: {}\n".format(star1(), star2()))
