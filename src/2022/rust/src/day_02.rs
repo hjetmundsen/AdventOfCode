906d091ff6541d62e8ca4bdf59d9f82b62f0b2d3
Henry Jetmundsen <hjetmundsen@atlassian.com>
2022-12-02 22:48:54 -0800

    Improve 2022 day 2

--- src/2022/rust/src/day_02.rs
+++ src/2022/rust/src/day_02.rs
@@ -5,79 +5,36 @@ fn star1() -> u32 {
     let lines: Vec<String> = utils::file_to_lines("../inputs/day_02.txt");
     
     let rules = HashMap::from([
-        ('A', 'Z'),
-        ('B', 'X'),
-        ('C', 'Y'),
-        ('X', 'C'),
-        ('Y', 'A'),
-        ('Z', 'B')
+        ("A X", 4),
+        ("A Y", 8),
+        ("A Z", 3),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 7),
+        ("C Y", 2),
+        ("C Z", 6),
     ]);
     
-    let letter_scores = HashMap::from([
-        ('X', 1),
-        ('Y', 2),
-        ('Z', 3)
-    ]);
-
-    let mut score = 0;
-    for line in lines {
-        let opponent = line.chars().nth(0).unwrap();
-        let mine = line.chars().nth(2).unwrap();
-        
-        score += letter_scores.get(&mine).unwrap();
-        
-        if rules.get(&mine).unwrap() == &opponent {
-            score += 6;
-        } else if rules.get(&opponent).unwrap() == &mine {
-            continue;
-        } else {
-            score += 3;
-        }
-    }
-    
-    score
+    lines.iter().map(|line| rules.get(line.as_str()).unwrap()).sum()
 }
 
 fn star2() -> u32 {
     let lines: Vec<String> = utils::file_to_lines("../inputs/day_02.txt");
-    
-    let beats = HashMap::from([
-        ('A', 'C'),
-        ('B', 'A'),
-        ('C', 'B'),
+
+    let rules = HashMap::from([
+        ("A X", 3),
+        ("A Y", 4),
+        ("A Z", 8),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 2),
+        ("C Y", 6),
+        ("C Z", 7),
     ]);
 
-    let loses = HashMap::from([
-        ('A', 'B'),
-        ('B', 'C'),
-        ('C', 'A'),
-    ]);
-    
-    let letter_scores = HashMap::from([
-        ('A', 1),
-        ('B', 2),
-        ('C', 3)
-    ]);
-
-    let mut score = 0;
-    for line in lines {
-        let opponent = line.chars().nth(0).unwrap();
-        let outcome = line.chars().nth(2).unwrap();
-        
-        if outcome == 'X' {
-            let mine = beats.get(&opponent).unwrap();
-            
-            score += letter_scores.get(mine).unwrap();
-        } else if outcome == 'Y' {
-            score += letter_scores.get(&opponent).unwrap() + 3;
-        } else {
-            let mine = loses.get(&opponent).unwrap();
-            
-            score += letter_scores.get(mine).unwrap() + 6;
-        }
-    }
-    
-    score
+    lines.iter().map(|line| rules.get(line.as_str()).unwrap()).sum()
 }
 
 pub fn main() {
